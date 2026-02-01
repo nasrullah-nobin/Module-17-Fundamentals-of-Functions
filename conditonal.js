239b59b458d65c80660a57cfc031add4b92d9b82
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-01 14:56:57 +0600

    re check coditionla in function

--- conditonal.js
+++ conditonal.js
@@ -1,29 +1,30 @@
-// function isEven (number){
-//     if(number %2 === 0){
-//         return true
-//     }
-//     else{
-//         return false
-//     }
-// }
-// const even = isEven(6)
-// console.log(even);
-// console.log(isEven(7));
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const even = isEven(6);
+console.log(even);
+console.log(isEven(7));
 
-// function isEven(number) {
-//   return number % 2 === 0 ? true : false;
-// }
-// console.log(isEven(4));
+function isEven(number) {
+  return number % 2 === 0 ? true : false;
+}
+console.log(isEven(4));
+console.log(isEven(9));
 
-// function isodd(number){
-//     return number%2===1? true: false;
-// }
-// console.log(isodd(2));
+function isodd(number) {
+  return number % 2 === 1 ? true : false;
+}
+console.log(isodd(2));
+console.log(isodd(1));
 
-// function isodd(number) {
-//   if (number % 2 === 1) {
-//     return true;
-//   }
-//   return false;
-// }
-// console.log(isodd(1));
+function isodd(number) {
+  if (number % 2 === 1) {
+    return true;
+  }
+  return false;
+}
+console.log(isodd(1));
