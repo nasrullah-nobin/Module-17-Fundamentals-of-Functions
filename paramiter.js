118e1d09b03a1072aa2229581a7158aab6d0d2a2
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-01 13:29:39 +0600

    reacap paramiter

--- paramiter.js
+++ paramiter.js
@@ -1,23 +1,18 @@
-function square(number) {
-  console.log("value of the number paramiter", number);
-  const borgo = number * number;
-  console.log("square of the given number is: ", borgo);
-}
-square(11);
-console.log("------------------");
-square(2);
-console.log("howwwwwwwwwwwwwwwwwwwwww");
-square(4);
+// function square(number) {
+//   console.log("value of the number paramiter", number);
+//   const borgo = number * number;
+//   console.log("square of the given number is: ", borgo);
+// }
+// square(11);
+// console.log("------------------");
+// square(2);
+// console.log("howwwwwwwwwwwwwwwwwwwwww");
+// square(4);
 
-function add(num1, num2) {
-  const sum = num1 + num2;
-  console.log(sum);
-}
-add(10, 10);
+// function add(num1, num2) {
+//   const sum = num1 + num2;
+//   console.log(sum);
+// }
+// add(10, 10);
 
-function addAll(a, b, c, d, e) {
-  const total = a + b + c + d + e;
-  console.log(a, b, c, d, e);
-  console.log(total);
-}
-addAll(2, 2, 2, 2);
+
