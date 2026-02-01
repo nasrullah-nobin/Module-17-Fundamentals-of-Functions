ad78a340ab3c6de9354de3038e75bcbd8c8ba719
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-01 14:46:27 +0600

    recap fundamental of function

--- recap.js
+++ recap.js
@@ -1,23 +1,23 @@
-// function payment(num1, num2) {
-//   const result = num1 + num2;
-//   return result;
-// }
-// const bil = payment(5, 50);
-// console.log("Your total bil :", bil);
+function payment(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+const bil = payment(5, 50);
+console.log("Your total bil :", bil);
 
-// function shortcut(num1, num2) {
-//   return num1 + num2;
-// }
-// const total = shortcut(20, 30);
-// console.log("Your pay Amount :", total);
+function shortcut(num1, num2) {
+  return num1 + num2;
+}
+const total = shortcut(20, 30);
+console.log("Your pay Amount :", total);
 
-// function doMath(num1, num2) {
-//   const sum = num1 + num2;
-//   const difference = num1 - num2;
-//   const multiply = sum * difference;
-//   const division = multiply / 2;
-//   return division;
-// }
-// const math = doMath(10, 5);
-// console.log(math);
+function doMath(num1, num2) {
+  const sum = num1 + num2;
+  const difference = num1 - num2;
+  const multiply = sum * difference;
+  const division = multiply / 2;
+  return division;
+}
+const math = doMath(10, 5);
+console.log(math);
 
