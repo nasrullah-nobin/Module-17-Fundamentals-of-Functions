f961038ecf9893bb1a24db3c153be637cd40da52
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-01 17:00:48 +0600

    recap varius paramiter

--- varius.js
+++ varius.js
@@ -1,34 +1,32 @@
-function evenSizedString(str) {
+function givenStringIsEven(str) {
   const size = str.length;
   console.log(str, size);
   if (size % 2 === 0) {
-    console.log("even size");
+    console.log("even number");
     return true;
   } else {
-    console.log("odd size");
+    console.log("odd number");
     return false;
   }
 }
-// console.log(evenSizedString("Dhaka"));
-// console.log(evenSizedString("faka"));
+const string = givenStringIsEven("even number of characters");
+const string1 = givenStringIsEven("I love programming ");
+console.log("even num:", string, "odd num", string1);
 
-function doubleOrTriple(number, doDouble) {
-  if (doDouble) {
-    const result = number * 2;
-    return result;
-  } else {
-    const result = number * 3;
-    return result;
+function doubleOrTriple(number, double) {
+  if (double) {
+    return number * 2;
   }
+  return number * 3;
 }
 
-// console.log(doubleOrTriple(5,true));
-// console.log(doubleOrTriple(5,false));
+const output = doubleOrTriple(5, true);
+const output1 = doubleOrTriple(5, false);
+console.log(output, output1);
 
-function getArraylength(number) {
-  const get = number.length;
-  return get;
+function numberOfElement(number) {
+  return number.length;
 }
-
-// console.log(getArraylength([1,3,4,5]));
-
+const array = [1, 2, 3, 4, 5, 67, 88];
+const output2 = numberOfElement(array);
+console.log(output2);
