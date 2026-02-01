d30e908b528b2739c78312b1caacf6a994b3acbe
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-01 14:04:21 +0600

    recap return

--- return.js
+++ return.js
@@ -1,15 +1,25 @@
-function tenTime(number) {
-  const gun = number * 10;
-  return gun;
-}
+// function tenTime(number) {
+//   const result = number * 10;
+//   return result;
+// }
 
-const result = tenTime(10);
-console.log(result);
+// const result = tenTime(10);
+// console.log(result);
 
 function division(number) {
   const div = number / 2;
   return div;
 }
 
-const retValue = division(50);
-console.log(retValue);
+const output = division(50);
+console.log(output);
+
+
+const output1 = division(15);
+console.log(output1);
+
+function deference(number){
+return number - 30;
+}
+const output2 = deference(50);
+console.log(output2)
