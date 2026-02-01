265b6efa1db9a40feb70e1ddb2d6b1ba6eb22e0c
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-01 17:24:37 +0600

    practice array operation

--- array.js
+++ array.js
@@ -1,10 +1,9 @@
-function sumOfArray(numbers){
-    let sum = 0;
-    for(const number of numbers){
-        sum = sum + number
-    }
-    return sum
+function sumOfArray(numbers) {
+  let sum = 0;
+  for (const number of numbers) {
+    sum = sum + number;
+  }
+  return sum;
 }
-const arr = [1,3,4,56,789,0];
-const sum = sumOfArray(arr);
-console.log(sum)
+const output = sumOfArray([1, 3, 4, 5, 67, 89]);
+console.log(output);
